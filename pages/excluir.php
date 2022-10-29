1bf86a6bae9ab5756af0981907078842588c14d7
WagnerSantos98 <wagner.jesus98@outlook.com>
2022-10-28 22:40:32 -0300

    cinema

--- pages/excluir.php
+++ pages/excluir.php
@@ -17,6 +17,42 @@ if(!empty($id)){
 		header("Location: ../pages/cartaz.php");
 	}
 }else{	
-	$_SESSION['msg'] = "<p style='color:red;'>Necessário selecionar um filme</p>";
+	$_SESSION['msg'] = "<p style='color:red;'>Necessário selecionar um filme.</p>";
+	header("Location: ../pages/cartaz.php");
+}
+
+//Exclusão de Show
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+if(!empty($id)){
+	$sql = "DELETE FROM tb_show WHERE id = '$id'";
+	$sql = mysqli_query($con, $sql);
+	if(mysqli_affected_rows($con)){
+		$_SESSION['msg'] = "<p style='color:green;'>Show excluído com sucesso.</p>";
+		header("Location: ../pages/cartaz.php");
+	}else{
+		
+		$_SESSION['msg'] = "<p style='color:red;'>Erro ao excluir o show.</p>";
+		header("Location: ../pages/cartaz.php");
+	}
+}else{	
+	$_SESSION['msg'] = "<p style='color:red;'>Necessário selecionar um show.</p>";
+	header("Location: ../pages/cartaz.php");
+}
+
+//Exclusão de Teatro
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+if(!empty($id)){
+	$sql = "DELETE FROM tb_teatro WHERE id = '$id'";
+	$sql = mysqli_query($con, $sql);
+	if(mysqli_affected_rows($con)){
+		$_SESSION['msg'] = "<p style='color:green;'>Peça de teatro excluído com sucesso.</p>";
+		header("Location: ../pages/cartaz.php");
+	}else{
+		
+		$_SESSION['msg'] = "<p style='color:red;'>Erro ao excluir a peça de teatro.</p>";
+		header("Location: ../pages/cartaz.php");
+	}
+}else{	
+	$_SESSION['msg'] = "<p style='color:red;'>Necessário selecionar uma peça de teatro.</p>";
 	header("Location: ../pages/cartaz.php");
 }
