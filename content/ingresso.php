f6930354e1a6d2aa0b508d164a98609a6b7b521b
WagnerSantos98 <wagner.jesus98@outlook.com>
2022-12-04 19:00:10 -0300

    cinema

--- content/ingresso.php
+++ content/ingresso.php
@@ -30,100 +30,28 @@ if(isset($_POST['realizar_venda'])){
      <!-- Compiled and minified CSS -->
      <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet" />
      <link href="https://unpkg.com/materialize-stepper@3.0.0-beta.1.0.1/dist/css/mstepper.min.css" rel="stylesheet" />
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
  
      <!--Material Icons-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link href="../assests/css/seat-charts.css">
  
     <!-- Compiled Javascript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
     <script src="https://unpkg.com/materialize-stepper@3.0.0-beta.1.0.1/dist/js/mstepper.min.js"></script>
 
+    
+
     <!--JS Jquery Seats-->
-    <link rel="stylesheet" href="../assests/css/seat-charts.css">
-    <script src="../assests/js/jquery.seat-charts.min.js"></script>
-    <script src="../assests/js/jquery.seat-charts.js"></script>
-    <title>Home Page</title>
+   
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="../assests/js/jquery.seat-charts.js"></script>
+    <script type="text/javascript" src="../assests/js/jquery.seat-charts.min.js"></script>
 <style>
     *{
         margin: 0;
         padding: 0;
-    }
-
-    body{
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-    }
-    .nav-wrapper{
-        margin-top: -65px;
-    }
-    .block{
-        height: 1200px;
-        background-color: #eee;
-        margin-bottom: 20px;
-        padding: 60px 0;
-        text-align: center;
-    }
-    .container .exibicao{
-        border-radius: 8px;
-        border: 3px solid #333;
-        
-    }
-    /*Back to top button*/
-    #button{
-        display: inline-block;
-        background-color: rgba(0, 0, 0, 0.7);
-        width: 50px;
-        height: 50px;
-        text-align: center;
-        border-radius: 4px;
-        position: fixed;
-        bottom: 30px;
-        right: 30px;
-        transition: background-color .3s,
-            opacity .5s, visibility .5s;
-        opacity: 0;
-        visibility: hidden;
-        z-index: 1000;
-    }
-    #button img{
-        position: absolute;
-        top: 50%;
-        right: 15px;
-        width: 20px;
-        transform: translateY(-50%);
-    }
-   
-    #button:hover{
-        cursor: pointer;
-        background-color: #333;
-    }
-    #button:active{
-        background-color: #555;
-    }
-    #button.show{
-        opacity: 1;
-        visibility: visible;
-    }
-
-    .btn,
-.btn-large,
-.btn-small,
-.btn-flat {
-  border-radius: 4px;
-  font-weight: 500;
-}
-
-.card:hover {
-  box-shadow: 0px 10px 35px 0px rgba(0, 0, 0, 0.18);
-}
-
-.card {
-  border-radius: 15px;
-  box-shadow: 0px 5px 25px 0px rgba(0, 0, 0, 0.15);
-}
+    }    
 </style>
 </head>
 
@@ -146,180 +74,146 @@ if(isset($_POST['realizar_venda'])){
     </ul>
 
     <div class="section grey lighten-5">
-        <div class="container">
-          <div class="row">
-              <h3 class="light center-align blue-text">Comprar Ingresso</h3>
-              <div class="card">
-                <div class="card-content">
-      
-                  <ul class="stepper parallel horizontal">
-                    <li class="step active">
-                      <div class="step-title waves-effect waves-dark">Ingressos</div>
-                      <div class="step-content">
-                        <div class="row">
-                        <img height='198' width='156' class="validate" src="../upload/<?php echo $row_filmes['arquivo']; ?>">
+      <div class="container">
+        <div class="row">
+            <h3 class="light center-align blue-text">Comprar Ingresso</h3>
+            <div class="card">
+              <div class="card-content">
+
+                <ul class="stepper parallel horizontal">
+                  <li class="step active">
+                    <div class="step-title waves-effect waves-dark">Ingressos</div>
+                    <div class="step-content">
+                      <div class="row">
+                      <img height='198' width='156' class="validate" src="../upload/<?php echo $row_filmes['arquivo']; ?>">
                         <p>Filme: <span id="title"></span><input id="titulo" hidden name="titulo" type="text" class="validate" value="<?php echo $row_filmes['titulo']; ?>"></p>
                         <p>Duração: <span id="timer"></span><input id="duracao" hidden name="duracao" type="text" class="validate" value="<?php echo $row_filmes['duracao']; ?>"></p>
                         <p>Data: <span>13/11/2022 - 19:00</span></p>
-                                    
-                        <p>
-                          <label>
-                            <input class="with-gap" name="group3" type="radio" checked />
-                            <span>Inteira R$10,00</span>
-                          </label>
-                        </p>
-                        <p>
-                          <label>
-                            <input class="with-gap" name="group3" type="radio"/>
-                            <span>Meia R$5,00</span>
-                          </label>
-                        </p>
-                        </div>
-                        <div class="step-actions">
-                          <button class="waves-effect waves-dark btn blue next-step" data-feedback="anyThing">CONTINUAR</button>
-                        </div>
                       </div>
-                    </li>
-                    <li class="step">
-                      <div class="step-title waves-effect waves-dark">Lugares</div>
-                      <div class="step-content">
-                        <div class="row">
-                          <!--Poltronas-->
-                            <div class="container-fluid">        
+                      <div class="step-actions">
+                        <button class="waves-effect waves-dark btn blue next-step" data-feedback="anyThing">Continuar</button>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="step">
+                    <div class="step-title waves-effect waves-dark">Lugares</div>
+                    <div class="step-content">
+                      <div class="row">
+                        <!--Poltronas-->
+                        <div class="container-fluid">        
                                 <div class="demo">
                                     <div id="seat-map">
                                     <div class="front">TELA</div>					
                                 </div>
                                 
                                 <div class="booking-details">
-                                    <p>Filme: <span id="title"></span><input id="titulo" hidden name="titulo" type="text" class="validate" value="<?php echo $row_filmes['titulo']; ?>"></p>
-                                    <p>Duração: <span id="timer"></span><input id="duracao" hidden name="duracao" type="text" class="validate" value="<?php echo $row_filmes['duracao']; ?>"></p>
+                                    <p>Filme: <span id="title_lugares"></span><input id="titulo_lug" hidden name="titulo" type="text" class="validate" value="<?php echo $row_filmes['titulo']; ?>"></p>
+                                    <p>Duração: <span id="timer_lugares"></span><input id="duracao" hidden name="duracao" type="text" class="validate" value="<?php echo $row_filmes['duracao']; ?>"></p>
                                     <p>Poltronas: </p>
                                     <ul id="selected-seats" name="poltronas"></ul>
                                     <p>Ingressos: <span id="counter" name="qtde">0</span></p>
                                     <p>Total: <b>R$<span id="total" name="total">0</span></b></p>
-                                                                                        
+                                    
+                                    
                                     <div id="legend"></div>
                                 </div>
                                 <div style="clear:both"></div>
                                 </div>
 
                             </div>
-                        </div>
-                        <div class="step-actions">
-                          <button class="waves-effect waves-dark btn blue next-step">CONTINUAR</button>
-                          <button class="waves-effect waves-dark btn-flat previous-step">VOLTAR</button>
-                        </div>
                       </div>
-                    </li>
-                    <li class="step">
-                      <div class="step-title waves-effect waves-dark">Pagamento</div>
-                      <div class="step-content">
-                        <div class="row">
-                          
-                        </div>
-                        <div class="step-actions">
-                          <button class="waves-effect waves-dark btn blue next-step">CONTINUAR</button>
-                          <button class="waves-effect waves-dark btn-flat previous-step">VOLTAR</button>
-                        </div>
+                      <div class="step-actions">
+                        <button class="waves-effect waves-dark btn blue next-step">Continuar</button>
+                        <button class="waves-effect waves-dark btn-flat previous-step">Voltar</button>
                       </div>
-                    </li>
-                    <li class="step">
-                        <div class="step-title waves-effect waves-dark">Conclusão</div>
-                        <div class="step-content">
-                          <div class="row">
-                            <div class="input-field col s12">
-                              <input id="nome" type="text" class="validate">
-                              <label for="nome">Nome</label>
-                            </div>
-                            <div class="input-field col s12">
-                              <input id="email" type="email" class="validate">
-                              <label for="email">Email</label>
-                            </div>
-                            <div class="input-field col s12">
-                              <input id="contato" type="text" class="validate" onkeypress="$(this).mask('(00) 00000-0009')">
-                              <label for="contato">Contato</label>
-                            </div>
-                          </div>
-                          <div class="step-actions">
-                            <button class="waves-effect waves-dark btn green">COMPRAR</button>
-                          </div>
-                        </div>
-                      </li>
-                  </ul>
-      
+                    </div>
+                  </li>
+                  <li class="step">
+                    <div class="step-title waves-effect waves-dark">Pagamento</div>
+                    <div class="step-content">
+                      <div class="row">
+                      <div id="test1" class="col s12">
+            <h6 class='header'>Formas de Pagamento</h6>
+            <div class="container">
+              <div class="row">
+                
+              <form class="col s12" method="POST" action="">
+              <div class="row">
+                <div class="input-field col s12">
+                <p>
+                  <label>
+                    <input name="pix" type="radio" disabled/>
+                    <span><i class="fa-brands fa-pix" style="color:#2ebdae"></i> PIX</span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="dinheiro" type="radio" checked />
+                    <span><i class="fas fa-sack-dollar"></i> Dinheiro</span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="boleto" type="radio" disabled/>
+                    <span><i class="fas fa-barcode"></i> Boleto Bancário</span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name="cartao" type="radio" disabled/>
+                    <span><i class="far fa-credit-card"></i> Cartão de Crédito</span>
+                  </label>
+                </p>
+                <p>Total R$ <span id="total" name="total" onblur="calcularValor()"></span></p>
+                <p><input class="validate" id="valor" placeholder="Informe o valor" onkeyup="calcularValor()"></p>
+                <p>Troco R$ <span id="resultado"></span>
                 </div>
+                
+            </div>     
               </div>
-          </div>
+            </div>
+                        </div>
+                      </div>
+                      <div class="step-actions">
+                        <button class="waves-effect waves-dark btn blue next-step">Continuar</button>
+                        <button class="waves-effect waves-dark btn-flat previous-step">Voltar</button>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="step">
+                    <div class="step-title waves-effect waves-dark">Conclusão</div>
+                    <div class="step-content">
+                    <div class="row">
+                        <div class="input-field col s12">
+                          <input id="password" name="password" type="password" class="validate" required>
+                          <label for="password">Your password</label>
+                        </div>
+                      </div>
+                      <div class="step-actions">
+                        <button class="waves-effect waves-dark btn blue next-step" data-feedback="noThing">Finalizar!</button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
         </div>
-      </div>  
-    
+      </div>
+    </div>
+
+      
+      <script href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.js"></script>
+      <link rel="stylesheet" href="../assests/css/seat-charts.css">
+    <script src="../assests/js/jquery.seat-charts.min.js"></script>
+    <script src="../assests/js/jquery.seat-charts.js"></script>
 <script>
-    //
-    function detalhesFilmes(){ 
-                              var titulo = document.getElementById('titulo').value; 
-                              document.getElementById('title').innerHTML = titulo;
-                              var duracao = document.getElementById('duracao').value; 
-                              document.getElementById('timer').innerHTML = duracao;
-                              var arquivo = document.getElementById('arquivo').value; 
-                              document.getElementById('arq').innerHTML = arquivo;    
-                          } 
-                          window.onload = detalhesFilmes();
-                          
-    //Menu
-    document.addEventListener('DOMContentLoaded', function() {
+   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
 
-    $("body").css("paddingTop", $(".nav-wrapper").innerHeight());
-
-    $(function(){
-        $(".nav-wrapper li a").click(function(e){
-            "use strict";
-            e.preventDefault();
-            $("html, body").animate(
-                {
-                    scrollTop: $("#" + $(this).data("scroll")).offset().top + 1,
-                },
-                1000
-            );
-        });
-
-            $(this)
-            .addClass("active")
-            .parent()
-            .siblings()
-            .find("a")
-            .removeClass("active");
-
-            $(window).scroll(function(){
-            $(".block").each(function(){
-                if($(window).scrollTop() >= $(this).offset().top){
-                    var blockId = $(this).attr("id");
-                    $(".nav-wrapper a").removeClass("active");
-                    $('.nav-wrapper li a[data-scroll="' + blockId + '"]').addClass("active");
-                }
-            });
-        });
-    });
-
-    //Back to top button
-    var btn = $('#button');
-
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 300){
-            btn.addClass('show');
-        }else{
-            btn.removeClass('show');
-        }
-    });
-
-    btn.on('click', function(e){
-        e.preventDefault();
-        $('html, body').animate({scrollTop:0},'500');
-    });
-
+  
     function anyThing(destroyFeedback) {
   setTimeout(function() {
     destroyFeedback(true);
@@ -331,11 +225,11 @@ function noThing(destroyFeedback) {
     destroyFeedback(true);
   }, 10000);
 }
-
 var stepperDiv = document.querySelector('.stepper');
 console.log(stepperDiv);
 var stepper = new MStepper(stepperDiv);
-
+            
+   
 var price = 10; //price
                       $(document).ready(function() {
                         var $cart = $('#selected-seats'), //Sitting Area
@@ -412,8 +306,25 @@ var price = 10; //price
                       }
 
 
-
+ 
 
 </script>
+<script>
+function detalhesFilmes(){ 
+  var titulo = document.getElementById('titulo').value; 
+  document.getElementById('title').innerHTML = titulo;
+  var duracao = document.getElementById('duracao').value; 
+  document.getElementById('timer').innerHTML = duracao;
+  var arquivo = document.getElementById('arquivo').value; 
+  document.getElementById('arq').innerHTML = arquivo;   
+  
+  var titulo_lug = document.getElementById('titulo_lug').value; 
+  document.getElementById('title_lugares').innerHTML = titulo_lug;
+  var duracao = document.getElementById('duracao').value; 
+  document.getElementById('timer').innerHTML = duracao;
+                          } 
+                          window.onload = detalhesFilmes(); 
+</script>
+
 </body>
 </html>
